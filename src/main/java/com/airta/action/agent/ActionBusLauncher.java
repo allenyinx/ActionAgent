732d89222ab52194b6b052e1336a816e65fd8ebc
allenyin <yinxinla@163.com>
2019-09-27 18:10:55 +0800

    init project

---
+++ src/main/java/com/airta/action/agent/ActionBusLauncher.java
@@ -0,0 +1,16 @@
+package com.airta.action.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author allenyin
+ */
+@SpringBootApplication
+public class ActionBusLauncher {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionBusLauncher.class, args);
+	}
+
+}
---
+++ src/test/java/com/airta/action/agent/ActionBusLauncherTests.java
@@ -0,0 +1,13 @@
+package com.airta.action.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionBusLauncherTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
