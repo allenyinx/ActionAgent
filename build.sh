b6fc8691f8ad2e908d1c686cbe5b9bc2ed012fc1
allenyin <yinxinla@163.com>
2019-10-11 21:14:31 +0800

    modify build script

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ Release=1.0
 timestamp=$(date +"%Y%m%d%H.%s")
 ImageGlobalTag=${Release}-${timestamp}
 
-mvn clean
-mvn package
+./mvnw clean
+./mvnw package
 
 docker build -t ${DockerCategory}/${NAME}:${ImageGlobalTag} -f Dockerfile . || exit 1
