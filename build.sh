6cb4b41975ee462e78562547deef37107996ffd0
allenyin <yinxinla@163.com>
2019-10-11 21:12:42 +0800

    modify build script

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+DockerCategory=airta
+NAME=airgent
+Release=1.0
+timestamp=$(date +"%Y%m%d%H.%s")
+ImageGlobalTag=${Release}-${timestamp}
+
 mvn clean
 mvn package
 
-docker build -t airta/airgent:1.0 .
+docker build -t ${DockerCategory}/${NAME}:${ImageGlobalTag} -f Dockerfile . || exit 1
