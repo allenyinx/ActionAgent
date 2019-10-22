fe818fce7c36347003ef2513e5dd8876e47d2c1e
allenyin <yinxinla@163.com>
2019-10-22 20:35:00 +0800

    add image statics badge and labels

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,20 @@ FROM airta/agent_base_publish:latest
 
 ARG JAR_FILE=target/agent-*.jar
 
+ARG BUILD_DATE
+ARG VCS_REF
+ARG VERSION
+
+LABEL org.label-schema.build-date=$BUILD_DATE \
+      org.label-schema.name="Airgent" \
+      org.label-schema.description="airta webdriver agent" \
+      org.label-schema.url="http://www.airta.co" \
+      org.label-schema.vcs-ref=$VCS_REF \
+      org.label-schema.vcs-url="https://github.com/allenyinx/ActionAgent" \
+      org.label-schema.vendor="airta group" \
+      org.label-schema.version=$VERSION \
+      org.label-schema.schema-version="1.0"
+
 COPY driver/chromedriver /home/airbot/chromedriver
 RUN sudo chmod +x /home/airbot/chromedriver
 COPY ${JAR_FILE} /home/airbot/airgent.jar
--- build.sh
+++ build.sh
@@ -10,7 +10,12 @@ ImageGlobalTag=${Release}-${timestamp}
 ./mvnw package -DskipTests
 
 docker pull airta/agent_base_publish:latest
-docker build --no-cache -t ${DockerCategory}/${NAME}:${ImageGlobalTag} -f Dockerfile . || exit 1
+docker build --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+             --build-arg VCS_REF=`git rev-parse --short HEAD` \
+             --build-arg VERSION=${ImageGlobalTag} \
+             -t ${DockerCategory}/${NAME}:${ImageGlobalTag} \
+             -f Dockerfile . || exit 1
+#docker build --no-cache -t ${DockerCategory}/${NAME}:${ImageGlobalTag} -f Dockerfile . || exit 1
 docker tag ${DockerCategory}/${NAME}:${ImageGlobalTag} ${DockerCategory}/${NAME}:latest
 docker push ${DockerCategory}/${NAME}:${ImageGlobalTag}
 docker push ${DockerCategory}/${NAME}:latest
